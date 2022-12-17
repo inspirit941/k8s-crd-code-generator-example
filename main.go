e2e7dc018580167b47dc678f44aee462b36abd7d
inspirit941 <inspirit941@gmail.com>
2022-12-17 16:46:24 +0900

    feat: main.go - controller 호출하는 함수

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"github.com/inspirit941/kluster/pkg/apis/inspirit941.dev/v1alpha1"
 	klient "github.com/inspirit941/kluster/pkg/client/clientset/versioned"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/inspirit941/kluster/pkg/client/informers/externalversions"
+	"github.com/inspirit941/kluster/pkg/controller"
 	"k8s.io/client-go/tools/clientcmd"
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 func main() {
@@ -31,16 +32,27 @@ func main() {
 		log.Printf("building config from flags, %s", err.Error())
 	}
 
-	// generated code 사용
+	//generated code 사용
 	klientset, err := klient.NewForConfig(config)
 	if err != nil {
 		log.Printf("getting klientset %s", err.Error())
 	}
-	fmt.Println(klientset)
-	// 이런 식으로 쓸 수 있다.
-	klusters, err := klientset.Inspirit941V1alpha1().Klusters("").List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		log.Printf(err.Error())
+	//fmt.Println(klientset)
+	//// 이런 식으로 쓸 수 있다.
+	//klusters, err := klientset.Inspirit941V1alpha1().Klusters("").List(context.Background(), metav1.ListOptions{})
+	//if err != nil {
+	//	log.Printf(err.Error())
+	//}
+	//fmt.Println(len(klusters.Items))
+
+	// informer를 호출하려면 informerFactory를 사용해야 함.
+	informerFactory := externalversions.NewSharedInformerFactory(klientset, 20*time.Minute) // resync 시간은 20분으로 정의.
+	// informer를 동작시키려면 chan이 필요.
+	ch := make(chan struct{})
+	c := controller.NewController(klientset, informerFactory.Inspirit941().V1alpha1().Klusters())
+
+	informerFactory.Start(ch)
+	if err := c.Run(ch); err != nil {
+		log.Printf("error running controller %s\n", err.Error())
 	}
-	fmt.Println(len(klusters.Items))
 }
